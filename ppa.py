d2fac5534bea1058c8a6f7e25754dfc2f9a841ae
daraholic <1112dara@gmail.com>
2021-07-23 12:53:38 +0900

    Second Deploy

--- ppa.py
+++ ppa.py
@@ -3,10 +3,9 @@ from flask import Flask
 app=Flask(__name__) #__name__代表目前執行的模組
 @app.route("/") #Decorator
 def home():
-    return "Hello Flask"
+    return "안녕 Flask"
 @app.route("/test") #Decorator
 def test():
-    return "Just Test" #處理網站路徑
+    return "テストだけ" #處理網站路徑
 if __name__=="__main__":
     app.run() #立刻啟動伺服器
-
