6bcef3bac50cee99532d0bd001396d8ce297b922
daraholic <1112dara@gmail.com>
2021-07-25 23:34:17 +0900

    Third Deploy

--- ppa.py
+++ ppa.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__) #__name__代表目前執行的模組
 @app.route("/") #Decorator
 def home():
-    return "안녕 Flask"
+    return "안녕~"
 @app.route("/test") #Decorator
 def test():
     return "テストだけ" #處理網站路徑
